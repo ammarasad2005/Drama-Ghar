aebbba2d36ce4dfb843af25eb3ccb6e732ddeeb4
Muhammad Ammar Asad <ammarasad321993@gmail.com>
2026-05-10 17:35:30 +0500

    fix: add youtube image domains to next.config.ts remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**', 
       },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/**', 
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**', 
+      },
     ],
   },
   output: 'standalone',
